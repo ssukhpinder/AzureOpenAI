c5afeb575869ae6e17548303cd323467df672549
Sukhpinder Singh <66723473+sukhsukhpinder@users.noreply.github.com>
2024-03-28 21:26:38 +0530

    Added Code generation code

---
+++ Azure.OpenAI.CodeGeneration/sample-code/function/function.cs
@@ -0,0 +1,12 @@
+namespace Function
+{
+    class Program
+    {
+        int AbsoluteSquare(int num1, int num2)
+        {
+            int result = Math.Abs(num1 - num2);
+            result *= result;
+            return result;
+        }
+    }
+}
